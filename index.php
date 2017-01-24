9b37f876e42bb852380ae4bd0e6f8a1873c2b0f2
priambudiH <siebudi18@gmail.com>
2017-01-24 12:11:31 +0700

    update index dan file profil

---
+++ index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<h1> aowkoawkaowkaokwaokwo </h1>
+</body>
+</html>
---
+++ profil.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<p>INI PROFIL</p>
+</body>
+</html>
